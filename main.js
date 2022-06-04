5f636d32c29d8330ada669be06a418b618627a5b
Francis Verissimo <francissv97@gmail.com>
2022-06-04 19:58:00 -0400

    finish STAGE 5

--- main.js
+++ main.js
@@ -5,6 +5,42 @@ onScroll();
 function onScroll() {
   changeNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activeMenuArCurrentSection("home");
+  activeMenuArCurrentSection("services");
+  activeMenuArCurrentSection("about");
+  activeMenuArCurrentSection("contact");
+}
+
+function activeMenuArCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verificar se seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = document.getElementById(`${section}`).offsetTop;
+  const sectionHeight = document.getElementById(`${section}`).offsetHeight;
+
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  // verificar se a base está abaixo da linha imáginaria alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  // final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = document.getElementById(`${section}`).getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function changeNavOnScroll() {
@@ -37,7 +73,10 @@ ScrollReveal({
   distance: "80px",
   duration: 600,
 }).reveal(`
-  #home header, #home .content, #home .numbers,
+  #home .col-a, #home .col-b, #home .numbers,
   #services header, #services .card,
-  #about header h4, #about header h2, #about .content p,
-  #about .content img`);
+  #about .col-a header h4, #about .col-a header h2,
+  #about .col-a .content p, #about .col-b,
+  #contact .col-a header, #contact .col-a .content li,
+  #contact .col-a .button, #contact .col-b,
+  footer .logo, footer p, footer .social-links`);
